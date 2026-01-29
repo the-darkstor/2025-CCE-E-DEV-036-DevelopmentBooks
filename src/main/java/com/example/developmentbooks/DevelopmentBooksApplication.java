dbaa96054dcd93c33feec87b6f6ac9380f352b67
Thales Storino <thales.storino@gmail.com>
2026-01-29 14:16:16 +0100

    initil commit

---
+++ src/main/java/com/example/developmentbooks/DevelopmentBooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.developmentbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopmentBooksApplication.class, args);
+    }
+
+}
